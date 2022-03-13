4d12b5a240bfdb5b0ab5b11f380a353e801a0c6b
Roberto Toledo <8cho8cho8cho@gmail.com>
2022-03-13 01:17:28 -0600

    v1.0
    Queue Interface Test

--- src/simuladorprocesos/SimuladorProcesos.java
+++ src/simuladorprocesos/SimuladorProcesos.java
@@ -5,6 +5,9 @@
  */
 package simuladorprocesos;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author roble
@@ -15,10 +18,23 @@ public class SimuladorProcesos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("HOLA MUNDO");
-        System.out.println("Prueba desde LINUX");
-        System.out.println("LinuxTest");
+        /*Creamos la Cola Indicando el tipo de dato
+        Link: https://yosoy.dev/colas-en-java/ */
+        Queue<Integer> cola=new LinkedList();
+        /*Insertamos datos*/
+            cola.offer(3);
+            cola.add(14);
+            cola.offer(12);
+            cola.add(7);
+            cola.offer(10);
+        /*Impresion de la Cola llena con los datos*/
+        System.out.println("Cola llena: " + cola);
+        /*Estructura repetitiva para desencolar*/
+        while(cola.poll()!=null){//Desencolamos y el valor se compara con null
+            System.out.println(cola.peek());//Muestra el nuevo Frente
+        }
+        /*Muestra null debido a que la cola ya esta vacia*/
+        System.out.println(cola.peek()); 
     }
     
 }
